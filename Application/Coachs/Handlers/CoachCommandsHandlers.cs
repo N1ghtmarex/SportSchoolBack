5b35e02ecc07ee826ead2897dd0b88ce9bfa2025
N1ghtmarex <andreyshe22@gmail.com>
2024-06-04 17:57:55 +0500

    Revert "file param"
    This reverts commit b9b8b49c65355a39524f305ab26302980a7dca9e.

--- Application/Coachs/Handlers/CoachCommandsHandlers.cs
+++ Application/Coachs/Handlers/CoachCommandsHandlers.cs
@@ -142,7 +142,7 @@ namespace Application.Coachs.Handlers
                 }
                 
 
-                using (Stream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                using (Stream fileStream = new FileStream(filePath, FileMode.Create))
                 {
                     request.Body.Image.CopyTo(fileStream);
                 }
