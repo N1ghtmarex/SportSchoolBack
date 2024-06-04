d10f17a9bed994e21845dc8242e81706b89ac9ac
N1ghtmarex <andreyshe22@gmail.com>
2024-06-04 15:13:11 +0500

    .

--- Application/Coachs/Handlers/CoachCommandsHandlers.cs
+++ Application/Coachs/Handlers/CoachCommandsHandlers.cs
@@ -131,7 +131,7 @@ namespace Application.Coachs.Handlers
             {
                 var imagesDirectory = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)?.ToString() ?? string.Empty, "SportSchool", "wwwroot", "users");
                 //var filePath = Path.Combine(imagesDirectory, $"{coach.ExternalId}.jpeg");
-                var filePath = Path.Combine("/var/www/html/assets/images/", $"{coach.ExternalId}.jpeg");
+                var filePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)?.ToString() ?? string.Empty, $"{coach.ExternalId}.jpeg");
 
                 if (File.Exists(filePath))
                 {
